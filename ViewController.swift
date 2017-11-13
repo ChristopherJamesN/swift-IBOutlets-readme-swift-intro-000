694977df382b8e8e1ba0ba5ea790d2ad9c4b8ab9
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-13 17:18:36 -0600

    Added IBActions.

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
 
+    @IBAction func helloWorld(_ sender: Any) {
+        textDisplay.text = "Hello, world!"
+        print("Pressed Hello World Button.")
+    }
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        textDisplay.text = "Goodbye, world!"
+        print("Pressed goodbye world button.")
+    }
+    @IBAction func unicorn(_ sender: Any) {
+        textDisplay.text="Unicorn!"
+        print("Pressed the unicorn button.")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
